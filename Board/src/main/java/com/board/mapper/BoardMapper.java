fb8309b0ab038a7ab23c4a2796aacf6cda8cabeb
gwidding <135992700+gwidding@users.noreply.github.com>
2023-11-27 14:14:24 +0900

    실수로 넣은 테스트 안의 패키지 수정

--- Board/src/main/java/com/board/mapper/BoardMapper.java
+++ Board/src/main/java/com/board/mapper/BoardMapper.java
@@ -1,14 +1,12 @@
 package com.board.mapper;
 
 import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
-
 import com.board.domain.BoardDTO;
 
 //매퍼로 만들 추상클래스인데 데이터베이스에 알려주도록 @Mapper
 @Mapper
-public interface BoradMapper {
+public interface BoardMapper {
 	// int를 boolean으로 해도 됨
 	public int insertBoard(BoardDTO params);
 	public BoardDTO selectBoardDetail(Long idx);
